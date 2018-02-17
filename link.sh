1c9ad94d8c2d95cc1309ee455ffc9e820470feb2
Mike <mike+github@technomonkey.net>
2018-02-17 15:48:18 -0800

    sourcing feels cleaner than symlinking

--- link.sh
+++ link.sh
@@ -1,2 +1,6 @@
-ln -s ~/git/dotfiles/nvim.vim ~/.config/nvim/init.vim
-ln -s ~/git/dotfiles/tmux.conf ~/.tmux.conf
+echo "export DOTFILES=~/git/dotfiles" > ~/.zshrc
+echo "source \${DOTFILES}/zshrc" >> ~/.zshrc
+
+echo "source \${DOTFILES}/nvim.vim" > ~/.config/nvim/init.vim
+echo "source \${DOTFILES}/tmux.conf" > ~/.tmux.conf
+
