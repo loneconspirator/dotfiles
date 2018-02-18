3e130a4c254752de35c45b3ac1a338b88bb88e0a
Mike McCracken <mike+github@technomonkey.net>
2018-02-18 09:51:06 -0800

    Include sourcing gitconfig

--- link.sh
+++ link.sh
@@ -1,6 +1,9 @@
 echo "export DOTFILES=~/git/dotfiles" > ~/.zshrc
+source ~/.zshrc
 echo "source \${DOTFILES}/zshrc" >> ~/.zshrc
 
 echo "source \${DOTFILES}/nvim.vim" > ~/.config/nvim/init.vim
 echo "source \${DOTFILES}/tmux.conf" > ~/.tmux.conf
 
+echo "[include]" > ~/.gitconfig
+echo "  path = ${DOTFILES}/gitconfig" >> ~/.gitconfig
