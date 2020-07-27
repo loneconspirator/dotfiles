c047f60de1a579a5f0f15775e96cd26a9fcf5db2
Mike McCracken <mike+github@technomonkey.net>
2020-07-27 16:10:11 -0700

    Alias common dev commands

--- zsh/dev_alias.zsh
+++ zsh/dev_alias.zsh
@@ -1,6 +1,10 @@
 alias fsd="foreman start -f Procfile.dev"
 alias bi="bundle install"
 alias gcm='git checkout master'
+alias ss='spring stop'
+alias tc='rm -rf tmp/cache'
+alias dbm='bundle exec rails db:migrate ; RAILS_ENV=test bundle exec rails db:migrate'
+alias dbr='bundle exec rails db:rollback ; RAILS_ENV=test bundle exec rails db:rollback'
 
 compdef g=git
 function g {
@@ -10,5 +14,3 @@ function g {
     git status --short --branch
   fi
 }
-
-alias gcm='git checkout master'
