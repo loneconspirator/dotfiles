15d6c2c26b600ecfeb3883af5161bba2e2da6bb7
Mike <mike+github@technomonkey.net>
2021-12-22 16:46:46 -0800

    Migrating test independently was causing trouble

--- zsh/dev_alias.zsh
+++ zsh/dev_alias.zsh
@@ -3,8 +3,9 @@ alias bi="bundle install"
 alias gcm='git checkout master'
 alias ss='spring stop'
 alias tc='rm -rf tmp/cache'
-alias dbm='bundle exec rails db:migrate ; RAILS_ENV=test bundle exec rails db:migrate'
-alias dbr='bundle exec rails db:rollback ; RAILS_ENV=test bundle exec rails db:rollback'
+alias dblt='RAILS_ENV=test bundle exec rails db:schema:load'
+alias dbm='bundle exec rails db:migrate ; dblt'
+alias dbr='bundle exec rails db:rollback ; dblt'
 
 alias cdip="cd ~/git/InPlay"
 
